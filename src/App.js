4c05a18e117ed73b9b8a7911ac52be0a40a36544
otep <stepan.kocis@educanet.cz>
2026-01-19 13:29:27 +0100

    home?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header.js"
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import {useState, useEffect} from "react";
+import Home from "./pages/Home";
+import Todolist from "./pages/Todolist";
+
+import {Link, Routes, Route, Router} from "react-router";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <article>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/todo"> Vydaje</Link>
+            </nav>
+
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/todo" element={<Todolist />}/>
+                </Routes>
+        </article>
+    )
+
 }
 
 export default App;
